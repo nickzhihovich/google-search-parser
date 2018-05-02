3b6bbb9b7635028fb77817b270a7a0f108cd0008
Unknown <marderer@bk.ru>
2018-05-02 23:50:21 +0300

    Add migrations for creation table queries and results

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180502193108) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "queries", force: :cascade do |t|
+    t.string "keyword"
+    t.string "search_engine"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "results", force: :cascade do |t|
+    t.string "website"
+    t.string "full_link"
+    t.string "position"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "query_id"
+  end
 
 end
